9210f6e25bdaa31f89d7f770613ed64d4bc72505
Leo Zhong <yunji@uber.com>
2020-12-15 14:40:58 -0800

    ZeroEvenOdd solve

--- zeroevenodd/zeroevenodd.go
+++ zeroevenodd/zeroevenodd.go
@@ -1,13 +1,19 @@
 package zeroevenodd
 
 import (
+	"context"
 	"fmt"
+	"golang.org/x/sync/semaphore"
 	"sync"
 )
 
 type ZeroEvenOdd struct {
 	n int
 	wg *sync.WaitGroup
+	sm0 *semaphore.Weighted
+	sm1 *semaphore.Weighted
+	sm2 *semaphore.Weighted
+	ctx context.Context
 }
 
 func (zeo *ZeroEvenOdd) zero() {
@@ -15,9 +21,15 @@ func (zeo *ZeroEvenOdd) zero() {
 	go func() {
 		defer zeo.wg.Done()
 		for i:=0; i<zeo.n; i++{
+			zeo.sm0.Acquire(zeo.ctx, 1)
 			fmt.Print(0)
-		}
 
+			if i & 1 == 1{
+				zeo.sm2.Release(1)
+			} else {
+				zeo.sm1.Release(1)
+			}
+		}
 	}()
 }
 
@@ -28,8 +40,10 @@ func (zeo *ZeroEvenOdd) even(){
 		count := zeo.n/2
 		number := 2
 		for i:= 0; i<count; i++ {
+			zeo.sm2.Acquire(zeo.ctx, 1)
 			fmt.Print(number)
 			number += 2
+			zeo.sm0.Release(1)
 		}
 	}()
 }
@@ -45,18 +59,30 @@ func (zeo *ZeroEvenOdd) odd(){
 		number := 1
 
 		for i:= 0; i<count; i++ {
+			zeo.sm1.Acquire(zeo.ctx, 1)
 			fmt.Print(number)
 			number += 2
+			zeo.sm0.Release(1)
 		}
 	}()
 }
 
+func (zeo *ZeroEvenOdd) init(){
+	zeo.sm1.Acquire(zeo.ctx,1)
+	zeo.sm2.Acquire(zeo.ctx, 1)
+}
+
 func Start(){
 	zeo := ZeroEvenOdd{
-		n:  15,
-		wg: &sync.WaitGroup{},
+		n:   15,
+		wg:  &sync.WaitGroup{},
+		sm0: semaphore.NewWeighted(1),
+		sm1: semaphore.NewWeighted(1),
+		sm2: semaphore.NewWeighted(1),
+		ctx: context.Background(),
 	}
 
+	zeo.init()
 	zeo.zero()
 	zeo.even()
 	zeo.odd()
