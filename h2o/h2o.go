086f727afcaaec796ce5339a0fcc3a8c92913b60
Leo Zhong <yunji@uber.com>
2020-12-15 20:05:20 -0800

    h2o with release semaphore

--- h2o/h2o.go
+++ h2o/h2o.go
@@ -13,6 +13,8 @@ type H2O struct {
 
 	semH *semaphore.Weighted
 	semO *semaphore.Weighted
+	semReleaseH *semaphore.Weighted
+	semReleaseO *semaphore.Weighted
 	ctx context.Context
 }
 
@@ -36,28 +38,35 @@ func (h *H2O) produceH(){
 	defer h.wg.Done()
 
 	h.semH.Acquire(h.ctx, 1)
+	h.semReleaseH.Release(1)
 	fmt.Print("H")
+	h.semReleaseO.Acquire(h.ctx,1)
 	h.semO.Release(1)
 }
 
 func (h *H2O) produceO(){
 	defer h.wg.Done()
 	h.semO.Acquire(h.ctx, 2)
+	h.semReleaseO.Release(2)
 	fmt.Print("O")
+	h.semReleaseH.Acquire(h.ctx,2)
 	h.semH.Release(2)
 }
 
 func (h *H2O) init(){
-	h.semO.Acquire(h.ctx, 2)
+	h.semReleaseH.Acquire(h.ctx, 2)
+	h.semReleaseO.Acquire(h.ctx, 2)
 }
 
 func Start(){
 	h := H2O{
-		n:    10,
-		wg:   &sync.WaitGroup{},
-		semH: semaphore.NewWeighted(2),
-		semO: semaphore.NewWeighted(2),
-		ctx: context.Background(),
+		n:     10,
+		wg:    &sync.WaitGroup{},
+		semH:  semaphore.NewWeighted(2),
+		semO:  semaphore.NewWeighted(2),
+		semReleaseH: semaphore.NewWeighted(2),
+		semReleaseO: semaphore.NewWeighted(2),
+		ctx:   context.Background(),
 	}
 
 	h.init()
