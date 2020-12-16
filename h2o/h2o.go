938c84e38a42f79bebb3401b01dd8b638ef08bb9
Leo Zhong <yunji@uber.com>
2020-12-15 16:19:13 -0800

    h2o slove always HHOHHOHHO...

--- h2o/h2o.go
+++ h2o/h2o.go
@@ -1,47 +1,66 @@
 package h2o
 
 import (
+	"context"
 	"fmt"
+	"golang.org/x/sync/semaphore"
 	"sync"
 )
 
 type H2O struct {
 	n int
 	wg *sync.WaitGroup
+
+	semH *semaphore.Weighted
+	semO *semaphore.Weighted
+	ctx context.Context
 }
 
 func (h *H2O) hydrogen(){
 	for i:=0; i<h.n; i++{
 		h.wg.Add(1)
-		go produceH(h.wg)
+		go h.produceH()
 		h.wg.Add(1)
-		go produceH(h.wg)
+		go h.produceH()
 	}
 }
 
 func (h *H2O) oxygen(){
 	for i:=0; i<h.n; i++{
 		h.wg.Add(1)
-		go produceO(h.wg)
+		go h.produceO()
 	}
 }
 
-func produceH(wg *sync.WaitGroup){
-	defer wg.Done()
+func (h *H2O) produceH(){
+	defer h.wg.Done()
+
+	h.semH.Acquire(h.ctx, 1)
 	fmt.Print("H")
+	h.semO.Release(1)
 }
 
-func produceO(wg *sync.WaitGroup){
-	defer wg.Done()
+func (h *H2O) produceO(){
+	defer h.wg.Done()
+	h.semO.Acquire(h.ctx, 2)
 	fmt.Print("O")
+	h.semH.Release(2)
+}
+
+func (h *H2O) init(){
+	h.semO.Acquire(h.ctx, 2)
 }
 
 func Start(){
 	h := H2O{
-		n:  10,
-		wg: &sync.WaitGroup{},
+		n:    10,
+		wg:   &sync.WaitGroup{},
+		semH: semaphore.NewWeighted(2),
+		semO: semaphore.NewWeighted(2),
+		ctx: context.Background(),
 	}
 
+	h.init()
 	h.hydrogen()
 	h.oxygen()
 
